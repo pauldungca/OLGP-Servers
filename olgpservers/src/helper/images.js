9e3ae462f426f14f52b67636944f5cc4d534df01
pauldungca <johnpauldungca0908@gmail.com>
2025-08-12 06:14:57 +0800

    account UI done!

--- olgpservers/src/helper/images.js
+++ olgpservers/src/helper/images.js
@@ -1,11 +1,13 @@
 import backgroundImage from "../assets/images/background.png";
 import OLGPlogo from "../assets/images/logo.png";
 import securityLogo from "../assets/images/securityLock.png";
+import accountImage from "../assets/images/accountImage.png";
 
 const images = {
   backgroundImage,
   OLGPlogo,
   securityLogo,
+  accountImage,
 };
 
 export default images;
--- olgpservers/src/pages/scheduler-pages/account.jsx
+++ olgpservers/src/pages/scheduler-pages/account.jsx
@@ -1,5 +1,91 @@
 import React from "react";
+import "../../assets/styles/account.css";
+import images from "../../helper/images";
+import Footer from "../../components/footer";
 
-export default function account() {
-  return <div>account</div>;
+export default function Account() {
+  return (
+    <div className="account-page-container">
+      <div className="account-header">
+        <div className="header-text-with-line">
+          <h3>ACCOUNT</h3>
+          <div className="header-line"></div>
+        </div>
+      </div>
+
+      <div className="account-content">
+        {/* Profile section */}
+        <div className="d-flex align-items-center mb-4">
+          <img
+            src={images.accountImage}
+            alt="Profile"
+            className="profile-image"
+          />
+          <div>
+            <h4 className="fw-bold fs-3 m-0">Juan Dela Cruz</h4>
+            <small className="text-muted">ID: 200890522</small>
+          </div>
+        </div>
+
+        {/* Form Section */}
+        <form>
+          {/* Row 1 */}
+          <div className="row mb-3">
+            <div className="col">
+              <label className="form-label">First Name</label>
+              <input type="text" className="form-control" disabled />
+            </div>
+            <div className="col">
+              <label className="form-label">Middle Name</label>
+              <input type="text" className="form-control" disabled />
+            </div>
+            <div className="col">
+              <label className="form-label">Last Name</label>
+              <input type="text" className="form-control" disabled />
+            </div>
+          </div>
+
+          {/* Row 2 */}
+          <div className="row mb-3">
+            <div className="col-9">
+              <label className="form-label">Address</label>
+              <input type="text" className="form-control" disabled />
+            </div>
+            <div className="col-3">
+              <label className="form-label">Sex</label>
+              <input type="text" className="form-control" disabled />
+            </div>
+          </div>
+
+          {/* Row 3 */}
+          <div className="row mb-4">
+            <div className="col-4">
+              <label className="form-label">Contact Number</label>
+              <input type="text" className="form-control" disabled />
+            </div>
+            <div className="col-8">
+              <label className="form-label">Email Address</label>
+              <input type="email" className="form-control" disabled />
+            </div>
+          </div>
+
+          {/* Buttons */}
+          <div className="d-flex justify-content-end gap-2">
+            <button type="button" className="btn btn-action">
+              Change Password
+            </button>
+            <button type="button" className="btn btn-action">
+              Edit Information
+            </button>
+            <button type="button" className="btn btn-action">
+              Backup Data
+            </button>
+          </div>
+        </form>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 }

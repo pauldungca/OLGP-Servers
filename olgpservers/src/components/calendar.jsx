b06b57d7ebe97c1413f997deda7cee91d984b403
pauldungca <johnpauldungca0908@gmail.com>
2025-08-14 05:30:27 +0800

    Dashboard UI done!

---
+++ olgpservers/src/components/calendar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Badge, Calendar } from "antd";
+
+const getListData = (value) => {
+  let listData = [];
+  switch (value.date()) {
+    case 8:
+      listData = [
+        { type: "warning", content: "This is warning event." },
+        { type: "success", content: "This is usual event." },
+      ];
+      break;
+    case 10:
+      listData = [
+        { type: "warning", content: "This is warning event." },
+        { type: "success", content: "This is usual event." },
+        { type: "error", content: "This is error event." },
+      ];
+      break;
+    case 15:
+      listData = [
+        { type: "warning", content: "This is warning event" },
+        { type: "success", content: "This is very long usual event......" },
+        { type: "error", content: "This is error event 1." },
+        { type: "error", content: "This is error event 2." },
+        { type: "error", content: "This is error event 3." },
+        { type: "error", content: "This is error event 4." },
+      ];
+      break;
+    default:
+  }
+  return listData || [];
+};
+
+const CalendarPage = () => {
+  const dateCellRender = (value) => {
+    const listData = getListData(value);
+    return (
+      <ul className="events">
+        {listData.map((item) => (
+          <li key={item.content}>
+            <Badge status={item.type} text={item.content} />
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const cellRender = (current, info) => {
+    if (info.type === "date") return dateCellRender(current);
+    return info.originNode;
+  };
+
+  return <Calendar cellRender={cellRender} />;
+};
+
+export default CalendarPage;

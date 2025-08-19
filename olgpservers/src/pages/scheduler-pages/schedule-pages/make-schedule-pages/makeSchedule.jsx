e9e1e56b44d96779f7215f8d8b83e23994ac2197
pauldungca <johnpauldungca0908@gmail.com>
2025-08-20 04:12:15 +0800

    created the directory folders of the schedule modules!

---
+++ olgpservers/src/pages/scheduler-pages/schedule-pages/make-schedule-pages/makeSchedule.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function makeSchedule() {
+  return <div>makeSchedule</div>;
+}
---
+++ olgpservers/src/pages/scheduler-pages/schedule-pages/open-schedule-pages/openSchedule.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function openSchedule() {
+  return <div>openSchedule</div>;
+}

7c26c979cc9c4d4f21426b449ead71109fa4d899
pauldungca <johnpauldungca0908@gmail.com>
2025-08-21 04:17:33 +0800

    View Schedule UI done!

--- olgpservers/src/pages/scheduler-pages/schedule-pages/view-schedule-pages/viewSchedule.jsx
+++ olgpservers/src/pages/scheduler-pages/schedule-pages/view-schedule-pages/viewSchedule.jsx
@@ -1,5 +1,50 @@
 import React from "react";
+import images from "../../../../helper/images";
+import { useNavigate } from "react-router-dom";
+import { createButtonCard } from "../../../../assets/scripts/member";
+import Footer from "../../../../components/footer";
 
-export default function viewSchedule() {
-  return <div>viewSchedule</div>;
+import "../../../../assets/styles/schedule.css";
+
+export default function ViewSchedule() {
+  const navigate = useNavigate();
+  const buttonCard = createButtonCard(images, navigate);
+  return (
+    <div className="schedule-page-container">
+      <div className="schedule-header">
+        <div className="header-text-with-line">
+          <h3>OPEN SCHEDULE</h3>
+          <div className="header-line"></div>
+        </div>
+      </div>
+      <div className="schedule-content">
+        <div className="schedule-cards-container">
+          {buttonCard({
+            department: "Altar Server",
+            parish: "View your schedule in the Altar Server Department.",
+            toPage: "/selectTime",
+          })}
+          {buttonCard({
+            department: "Eucharistic Minister",
+            parish:
+              "View your schedule in the Eucharistic Minister Department.",
+            toPage: "/selectTime",
+          })}
+          {buttonCard({
+            department: "Choir",
+            parish: "View your schedule in the Choir Department.",
+            toPage: "/selectTime",
+          })}
+          {buttonCard({
+            department: "Lector Commentator",
+            parish: "View your schedule in the Lector Commentator Department.",
+            toPage: "/selectTime",
+          })}
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 }

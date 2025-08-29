ce19e11e572655fcd12fb7070a342c1f9db393f4
pauldungca <johnpauldungca0908@gmail.com>
2025-08-29 23:57:05 +0800

    created the directory structure!

---
+++ olgpservers/src/pages/secretary-pages/account-pages/changePasswordAccount.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function changePasswordAccount() {
+  return <div>changePasswordAccount</div>;
+}
---
+++ olgpservers/src/pages/secretary-pages/notification-pages/viiewNotfication.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function viiewNotfication() {
+  return <div>viiewNotfication</div>;
+}

5e0197459273f8cf0fb0ba734b5f7da85334f05b
pauldungca <johnpauldungca0908@gmail.com>
2025-09-01 20:30:48 +0800

    Parish Secretary Notification UI done!

--- olgpservers/src/pages/scheduler-pages/schedule-pages/view-schedule-pages/updateSchedule.jsx
+++ olgpservers/src/pages/scheduler-pages/schedule-pages/view-schedule-pages/updateSchedule.jsx
@@ -56,262 +56,229 @@ export default function UpdateSchedule() {
           <div className="header-line"></div>
         </div>
       </div>
-      <div className="schedule-content container">
-        <div className="row g-4">
-          <div className="col-12">
-            <div className="schedule-card">
-              <div className="schedule-card-header">
-                <strong>April 6, 2025 | Sunday Mass</strong>
+
+      <div className="schedule-content">
+        <div className="update-schedule-wrapper">
+          <div className="update-schedule-header">
+            April 6, 2025 | Sunday Mass
+          </div>
+
+          <div className="update-schedule-body">
+            {/* Left Column */}
+            <div className="schedule-col no-schedule">
+              <img src={image.updateStatusImage} alt="No Schedule" />
+              <p>You don’t have a schedule here.</p>
+            </div>
+
+            {/* Middle Column */}
+            <div className="schedule-col assigned-group">
+              <div className="assigned-box">
+                <p className="assigned-label">Assigned Group:</p>
+                <h3 className="group-name">Group 2</h3>
               </div>
-              <div className="row text-center">
-                <div className="col-md-4 col-12 border-end ">
-                  <img
-                    src={image.updateStatusImage}
-                    alt="Calendar"
-                    className="schedule-icon"
-                  />
-                  <p>You don't have a schedule here.</p>
-                </div>
 
-                {/* Column 2 - Updated based on clearer image */}
-                <div className="col-md-4 col-12 border-end assigned-group-column">
-                  <div className="assigned-group-header">
-                    <strong>Assigned Group:</strong>
-                  </div>
-                  <div className="group-name">Group 2</div>
-                  <div className="mass-details">2nd Mass | 8:30 AM</div>
-                  <div className="action-buttons">
-                    <div className="d-flex justify-content-center">
-                      <button className="btn export-btn me-3">Export</button>
+              <p className="mass-time">2nd Mass | 8:30 AM</p>
 
-                      <button className="btn print-btn">Print</button>
-                    </div>
-                  </div>
-                  <div className="cancel-schedule">
-                    <button
-                      className="btn schedule-btn"
-                      onMouseEnter={() =>
-                        setHoveredBtn((prev) => ({ ...prev, btn2: true }))
-                      }
-                      onMouseLeave={() =>
-                        setHoveredBtn((prev) => ({ ...prev, btn2: false }))
-                      }
-                      onClick={navToCancel}
-                    >
-                      <img
-                        src={
-                          hoveredBtn.btn2
-                            ? image.noButtonHoverImage
-                            : image.noButtonImage
-                        }
-                        alt={notAvailableText}
-                        className="schedule-btn-icon"
-                      />
-                      {notAvailableText}
-                    </button>
-                  </div>
-                </div>
-
-                <div className="col-md-4 col-12">
-                  <img
-                    src={image.updateStatusImage}
-                    alt="Calendar"
-                    className="schedule-icon"
-                  />
-                  <p>You don't have a schedule here.</p>
-                </div>
+              <div className="action-buttons justify-content-center">
+                {<button className="btn export-btn">Export</button>}
+                {<button className="btn print-btn">Print</button>}
               </div>
+
+              <button
+                className="btn cancel-btn"
+                onMouseEnter={() =>
+                  setHoveredBtn((prev) => ({ ...prev, btn2: true }))
+                }
+                onMouseLeave={() =>
+                  setHoveredBtn((prev) => ({ ...prev, btn2: false }))
+                }
+                onClick={navToCancel}
+              >
+                <img
+                  src={
+                    hoveredBtn.btn2
+                      ? image.noButtonHoverImage
+                      : image.noButtonImage
+                  }
+                  alt="Cancel Schedule"
+                  className="cancel-btn-icon"
+                />
+                {notAvailableText}
+              </button>
+            </div>
+
+            {/* Right Column */}
+            <div className="schedule-col no-schedule">
+              <img src={image.updateStatusImage} alt="No Schedule" />
+              <p>You don’t have a schedule here.</p>
             </div>
           </div>
-          <div className="col-12">
-            <div className="schedule-card">
-              <div className="schedule-card-header">
-                <strong>April 6, 2025 | Sunday Mass</strong>
-              </div>
-              <div className="row text-center">
-                <div className="col-md-4 col-12 border-end ">
-                  <img
-                    src={image.updateStatusImage}
-                    alt="Calendar"
-                    className="schedule-icon"
-                  />
-                  <p>You don't have a schedule here.</p>
-                </div>
+        </div>
+        <div className="update-schedule-wrapper">
+          <div className="update-schedule-header">
+            April 6, 2025 | Sunday Mass
+          </div>
 
-                {/* Column 2 - Updated based on clearer image */}
-                <div className="col-md-4 col-12 border-end assigned-group-column">
-                  <div className="assigned-group-header">
-                    <strong>Assigned Group:</strong>
-                  </div>
-                  <div className="group-name">Group 2</div>
-                  <div className="mass-details">2nd Mass | 8:30 AM</div>
-                  <div className="action-buttons">
-                    <div className="d-flex justify-content-center">
-                      <button className="btn export-btn me-3">Export</button>
-                      <button className="btn print-btn">Print</button>
-                    </div>
-                  </div>
-                  <div className="cancel-schedule">
-                    <button
-                      className="btn schedule-btn"
-                      onMouseEnter={() =>
-                        setHoveredBtn((prev) => ({ ...prev, btn2: true }))
-                      }
-                      onMouseLeave={() =>
-                        setHoveredBtn((prev) => ({ ...prev, btn2: false }))
-                      }
-                    >
-                      <img
-                        src={
-                          hoveredBtn.btn2
-                            ? image.noButtonHoverImage
-                            : image.noButtonImage
-                        }
-                        alt={notAvailableText}
-                        className="schedule-btn-icon"
-                      />
-                      {notAvailableText}
-                    </button>
-                  </div>
-                </div>
+          <div className="update-schedule-body">
+            {/* Left Column */}
+            <div className="schedule-col no-schedule">
+              <img src={image.updateStatusImage} alt="No Schedule" />
+              <p>You don’t have a schedule here.</p>
+            </div>
 
-                <div className="col-md-4 col-12">
-                  <img
-                    src={image.updateStatusImage}
-                    alt="Calendar"
-                    className="schedule-icon"
-                  />
-                  <p>You don't have a schedule here.</p>
-                </div>
+            {/* Middle Column */}
+            <div className="schedule-col assigned-group">
+              <div className="assigned-box">
+                <p className="assigned-label">Assigned Group:</p>
+                <h3 className="group-name">Group 2</h3>
               </div>
+
+              <p className="mass-time">2nd Mass | 8:30 AM</p>
+
+              <div className="action-buttons justify-content-center">
+                {<button className="btn export-btn">Export</button>}
+                {<button className="btn print-btn">Print</button>}
+              </div>
+
+              <button
+                className="btn cancel-btn"
+                onMouseEnter={() =>
+                  setHoveredBtn((prev) => ({ ...prev, btn2: true }))
+                }
+                onMouseLeave={() =>
+                  setHoveredBtn((prev) => ({ ...prev, btn2: false }))
+                }
+                onClick={navToCancel}
+              >
+                <img
+                  src={
+                    hoveredBtn.btn2
+                      ? image.noButtonHoverImage
+                      : image.noButtonImage
+                  }
+                  alt="Cancel Schedule"
+                  className="cancel-btn-icon"
+                />
+                {notAvailableText}
+              </button>
+            </div>
+
+            {/* Right Column */}
+            <div className="schedule-col no-schedule">
+              <img src={image.updateStatusImage} alt="No Schedule" />
+              <p>You don’t have a schedule here.</p>
             </div>
           </div>
-          <div className="col-12">
-            <div className="schedule-card">
-              <div className="schedule-card-header">
-                <strong>April 6, 2025 | Sunday Mass</strong>
+        </div>
+        <div className="update-schedule-wrapper">
+          <div className="update-schedule-header">
+            April 6, 2025 | Sunday Mass
+          </div>
+
+          <div className="update-schedule-body">
+            {/* Left Column */}
+            <div className="schedule-col no-schedule">
+              <img src={image.updateStatusImage} alt="No Schedule" />
+              <p>You don’t have a schedule here.</p>
+            </div>
+
+            {/* Middle Column */}
+            <div className="schedule-col assigned-group">
+              <div className="assigned-box">
+                <p className="assigned-label">Assigned Group:</p>
+                <h3 className="group-name">Group 2</h3>
               </div>
-              <div className="row text-center">
-                <div className="col-md-4 col-12 border-end ">
-                  <img
-                    src={image.updateStatusImage}
-                    alt="Calendar"
-                    className="schedule-icon"
-                  />
-                  <p>You don't have a schedule here.</p>
-                </div>
 
-                {/* Column 2 - Updated based on clearer image */}
-                <div className="col-md-4 col-12 border-end assigned-group-column">
-                  <div className="assigned-group-header">
-                    <strong>Assigned Group:</strong>
-                  </div>
-                  <div className="group-name">Group 2</div>
-                  <div className="mass-details">2nd Mass | 8:30 AM</div>
-                  <div className="action-buttons">
-                    <div className="d-flex justify-content-center">
-                      <button className="btn export-btn me-3">Export</button>
+              <p className="mass-time">2nd Mass | 8:30 AM</p>
 
-                      <button className="btn print-btn">Print</button>
-                    </div>
-                  </div>
-                  <div className="cancel-schedule">
-                    <button
-                      className="btn schedule-btn"
-                      onMouseEnter={() =>
-                        setHoveredBtn((prev) => ({ ...prev, btn2: true }))
-                      }
-                      onMouseLeave={() =>
-                        setHoveredBtn((prev) => ({ ...prev, btn2: false }))
-                      }
-                    >
-                      <img
-                        src={
-                          hoveredBtn.btn2
-                            ? image.noButtonHoverImage
-                            : image.noButtonImage
-                        }
-                        alt={notAvailableText}
-                        className="schedule-btn-icon"
-                      />
-                      {notAvailableText}
-                    </button>
-                  </div>
-                </div>
-
-                <div className="col-md-4 col-12">
-                  <img
-                    src={image.updateStatusImage}
-                    alt="Calendar"
-                    className="schedule-icon"
-                  />
-                  <p>You don't have a schedule here.</p>
-                </div>
+              <div className="action-buttons justify-content-center">
+                {<button className="btn export-btn">Export</button>}
+                {<button className="btn print-btn">Print</button>}
               </div>
+
+              <button
+                className="btn cancel-btn"
+                onMouseEnter={() =>
+                  setHoveredBtn((prev) => ({ ...prev, btn2: true }))
+                }
+                onMouseLeave={() =>
+                  setHoveredBtn((prev) => ({ ...prev, btn2: false }))
+                }
+                onClick={navToCancel}
+              >
+                <img
+                  src={
+                    hoveredBtn.btn2
+                      ? image.noButtonHoverImage
+                      : image.noButtonImage
+                  }
+                  alt="Cancel Schedule"
+                  className="cancel-btn-icon"
+                />
+                {notAvailableText}
+              </button>
+            </div>
+
+            {/* Right Column */}
+            <div className="schedule-col no-schedule">
+              <img src={image.updateStatusImage} alt="No Schedule" />
+              <p>You don’t have a schedule here.</p>
             </div>
           </div>
-          <div className="col-12">
-            <div className="schedule-card">
-              <div className="schedule-card-header">
-                <strong>April 6, 2025 | Sunday Mass</strong>
+        </div>
+        <div className="update-schedule-wrapper">
+          <div className="update-schedule-header">
+            April 6, 2025 | Sunday Mass
+          </div>
+
+          <div className="update-schedule-body">
+            {/* Left Column */}
+            <div className="schedule-col no-schedule">
+              <img src={image.updateStatusImage} alt="No Schedule" />
+              <p>You don’t have a schedule here.</p>
+            </div>
+
+            {/* Middle Column */}
+            <div className="schedule-col assigned-group">
+              <div className="assigned-box">
+                <p className="assigned-label">Assigned Group:</p>
+                <h3 className="group-name">Group 2</h3>
               </div>
-              <div className="row text-center">
-                <div className="col-md-4 col-12 border-end ">
-                  <img
-                    src={image.updateStatusImage}
-                    alt="Calendar"
-                    className="schedule-icon"
-                  />
-                  <p>You don't have a schedule here.</p>
-                </div>
 
-                {/* Column 2 - Updated based on clearer image */}
-                <div className="col-md-4 col-12 border-end assigned-group-column">
-                  <div className="assigned-group-header">
-                    <strong>Assigned Group:</strong>
-                  </div>
-                  <div className="group-name">Group 2</div>
-                  <div className="mass-details">2nd Mass | 8:30 AM</div>
-                  <div className="action-buttons">
-                    <div className="d-flex justify-content-center">
-                      <button className="btn export-btn me-3">Export</button>
+              <p className="mass-time">2nd Mass | 8:30 AM</p>
 
-                      <button className="btn print-btn">Print</button>
-                    </div>
-                  </div>
-                  <div className="cancel-schedule">
-                    <button
-                      className="btn schedule-btn"
-                      onMouseEnter={() =>
-                        setHoveredBtn((prev) => ({ ...prev, btn2: true }))
-                      }
-                      onMouseLeave={() =>
-                        setHoveredBtn((prev) => ({ ...prev, btn2: false }))
-                      }
-                    >
-                      <img
-                        src={
-                          hoveredBtn.btn2
-                            ? image.noButtonHoverImage
-                            : image.noButtonImage
-                        }
-                        alt={notAvailableText}
-                        className="schedule-btn-icon"
-                      />
-                      {notAvailableText}
-                    </button>
-                  </div>
-                </div>
-
-                <div className="col-md-4 col-12">
-                  <img
-                    src={image.updateStatusImage}
-                    alt="Calendar"
-                    className="schedule-icon"
-                  />
-                  <p>You don't have a schedule here.</p>
-                </div>
+              <div className="action-buttons justify-content-center">
+                {<button className="btn export-btn">Export</button>}
+                {<button className="btn print-btn">Print</button>}
               </div>
+
+              <button
+                className="btn cancel-btn"
+                onMouseEnter={() =>
+                  setHoveredBtn((prev) => ({ ...prev, btn2: true }))
+                }
+                onMouseLeave={() =>
+                  setHoveredBtn((prev) => ({ ...prev, btn2: false }))
+                }
+                onClick={navToCancel}
+              >
+                <img
+                  src={
+                    hoveredBtn.btn2
+                      ? image.noButtonHoverImage
+                      : image.noButtonImage
+                  }
+                  alt="Cancel Schedule"
+                  className="cancel-btn-icon"
+                />
+                {notAvailableText}
+              </button>
+            </div>
+
+            {/* Right Column */}
+            <div className="schedule-col no-schedule">
+              <img src={image.updateStatusImage} alt="No Schedule" />
+              <p>You don’t have a schedule here.</p>
             </div>
           </div>
         </div>
--- olgpservers/src/pages/secretary-pages/schedule-pages/view-schedule-pages/cancelSchedule.jsx
+++ olgpservers/src/pages/secretary-pages/schedule-pages/view-schedule-pages/cancelSchedule.jsx
@@ -5,6 +5,7 @@ import icon from "../../../../helper/icon";
 import Footer from "../../../../components/footer";
 
 import "../../../../assets/styles/schedule.css";
+import "../../../../assets/styles/cancelSchedule.css";
 
 export default function cancelSchedule() {
   return (
@@ -43,7 +44,37 @@ export default function cancelSchedule() {
           <div className="header-line"></div>
         </div>
       </div>
-      <div className="schedule-content"></div>
+      <div className="schedule-content container">
+        <div className="cancel-schedule-card shadow-sm p-4 rounded">
+          <h4 className="mb-4">Cancel Schedule</h4>
+
+          <p>
+            <strong>Date:</strong> April 6, 2025
+          </p>
+          <p>
+            <strong>Mass:</strong> Sunday Mass
+          </p>
+          <p>
+            <strong>Group:</strong> Group 2
+          </p>
+          <p>
+            <strong>Time:</strong> 2nd Mass | 8:30 AM
+          </p>
+
+          <div className="mb-3 row align-items-center reason-row">
+            <label className="col-sm-2 col-form-label">
+              <strong>Reason:</strong>
+            </label>
+            <div className="col-sm-10 d-flex align-items-center">
+              <button className="btn btn-attach">Attach File</button>
+            </div>
+          </div>
+
+          <div className="text-center mt-4">
+            <button className="btn btn-confirm w-100">Confirm</button>
+          </div>
+        </div>
+      </div>
       <div>
         <Footer />
       </div>

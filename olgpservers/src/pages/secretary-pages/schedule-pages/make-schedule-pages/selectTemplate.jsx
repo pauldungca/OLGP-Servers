25440fee35abb95daa4144a947029d2997cae44e
pauldungca <johnpauldungca0908@gmail.com>
2025-09-02 18:30:06 +0800

    select template UI done!

--- olgpservers/src/pages/secretary-pages/schedule-pages/make-schedule-pages/selectTemplate.jsx
+++ olgpservers/src/pages/secretary-pages/schedule-pages/make-schedule-pages/selectTemplate.jsx
@@ -1,29 +1,64 @@
 import React from "react";
-import { useNavigate } from "react-router";
+import image from "../../../../helper/images";
 import Footer from "../../../../components/footer";
 
 import "../../../../assets/styles/schedule.css";
+import "../../../../assets/styles/selectTemplate.css";
 
 export default function SelectTemplate() {
-  const navigate = useNavigate();
+  // Template Data (can come from props, API, etc.)
+  const templates = [
+    { id: 1, title: "Christening Mass", image: image.OLGPlogo },
+    { id: 2, title: "Wedding Mass", image: image.OLGPlogo },
+    { id: 3, title: "Funeral Mass", image: image.OLGPlogo },
+    { id: 4, title: "Sunday Mass", image: image.OLGPlogo },
+  ];
 
-  const handleCardClick = () => {
-    navigate("/createTemplate");
-  };
   return (
-    <div className="schedule-page-container">
+    <div className="schedule-page-container d-flex flex-column">
+      {/* Header */}
       <div className="schedule-header">
         <div className="header-text-with-line">
           <h3>MAKE SCHEDULE</h3>
           <div className="header-line"></div>
         </div>
       </div>
-      <div className="schedule-content">
-        <button onClick={handleCardClick}>Next Page</button>
+
+      {/* Content Area */}
+      <div className="schedule-content container flex-grow-1">
+        <div className="row">
+          {templates.map((template) => (
+            <div key={template.id} className="col-md-4 mb-4">
+              <div className="template-card">
+                <div className="template-header">
+                  <span className="template-title">{template.title}</span>
+                  <div className="template-icons">
+                    <i className="bi bi-pencil-square edit-icon"></i>
+                    <i className="bi bi-trash delete-icon"></i>
+                  </div>
+                </div>
+                <div className="template-body">
+                  <img
+                    src={template.image}
+                    alt={template.title}
+                    className="template-image"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        <Footer />
+
+      {/* Always Bottom Button */}
+      <div className="create-btn-container">
+        <button className="btn btn-primary create-btn">
+          <i className="bi bi-plus-circle"></i> Create Mass Schedule Template
+        </button>
       </div>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
